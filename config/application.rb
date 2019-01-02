baa2092a5fc52632b282ea4b7cff18c8a6385bfe
Rogelio Sevilla <rogelio@junglescout.com>
2019-01-01 18:23:26 -0600

    several fixes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module DockerizedApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.log_level = :debug
+    config.log_tags = [:subdomain, :uuid]
+    config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+        { namespace: "#{ENV['POSTGRES_ENV_POSTGRES_USER']}::cache" }
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Feedback.create!(message: 'focas')
+Feedback.create!(message: 'osos')
+Feedback.create!(message: 'morsas')
